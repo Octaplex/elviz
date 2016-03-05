7bbba62284def1366f01db1a360e0c6732087dc1
jerryTheMonster <Jerrymeng100@gmail.com>
2016-03-05 13:01:41 -0500

    Started coil physics
    Added constructor, and two kinds of elliptic integrals for magnetic
    field.

--- physics.py
+++ physics.py
@@ -1,4 +1,7 @@
 import math
+import numpy as np
+import scipy as sp
+import scipy.integrate as integrate
 
 #constants
 muNot = 4*math.pi*10**-7
@@ -29,6 +32,25 @@ class Wire(BInducer):
     def bfield_draw(self, x, y, z):
         B = self.bfield_strength(x, y, z)
         arrow(pos=(x,y,z), axis=tuple(B))
+
+class Coil(BInducer):
+
+    def __init__(self, center, radius, norm, I):
+        self.center = center
+        self.radius = radius
+        self.norm = norm
+        self.I = I
+
+    #1st and 2nd kind of elliptic integrals, K(k) and E(k) respectively
+    #http://www.mhtlab.uwaterloo.ca/courses/me755/web_chap3.pdf
+    def K(k):
+        return integrate.quad(lambda t: ((1-t**2)*(1-k**2*t**2))**-0.5, 0, 1)[0]
+
+    def E(k):
+        return integrate.quad(lambda t: ((1-k**2*t**2)/(1-t**2))**0.5, 0, 1)[0]
+    
+    def bfield_strength(self, x, y, z):
+        return None #WIP
 
 class Field:
     def __call__(self, x, y, z): raise NotImplementedError
