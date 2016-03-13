02a9a712c039caa0deefe4e78dce894928e35b67
jerryTheMonster <Jerrymeng100@gmail.com>
2016-03-12 22:52:44 -0500

    Updated coil math
    Unpacking error in physics; not sure where this is from

--- elviz.py
+++ elviz.py
@@ -4,9 +4,9 @@ from visual import *
 from physics import BField
 from shapes import Wire, Coil
 
-#w = Wire(vector(0, 0, 0), vector(0, 10, 0), 100000000)
+w = Wire(vector(0, 0, 0), vector(0, 10, 0), 100000000)
 
-r = Coil(vector(0, 0, 0), 10, vector(0, 1, 0), 10000000)
+r = Coil(vector(0, 0, 0), 10, vector(0, 0, 1), 10000000)
 
 B = BField()
 B.add_inducer(r)
--- shapes.py
+++ shapes.py
@@ -69,26 +69,30 @@ class Coil(BInducer):
         self.normal = normal
         self.I = I
         # rotation matrices for this coil, rotating so norm becomes z axis
-        rotate = self.find_rotmatrix(normal, vector(0, 0, 1))
-        antiRotate = inv(rotate)
+        self.rotate = self.find_rotmatrix(normal, vector(0, 0, 1))
+        self.antiRotate = la.inv(self.rotate)
 
         ring(pos = center, axis = normal, radius = radius, thickness = 0.1)
 
 
     # 1st and 2nd kind of elliptic integrals, K(k) and E(k) respectively
     # http://www.mhtlab.uwaterloo.ca/courses/me755/web_chap3.pdf
-    def K(k):
+    def K(self, k):
         return integrate.quad(lambda t: ((1-t**2)*(1-k**2*t**2))**-0.5, 0, 1)[0]
 
-    def E(k):
+    def E(self, k):
         return integrate.quad(lambda t: ((1-k**2*t**2)/(1-t**2))**0.5, 0, 1)[0]
 
     def find_rotmatrix(self, a, b):
+        if a.x==0 and a.y==0: #if no rotation needed
+            return np.matrix([[1,0,0],
+                              [0,1,0],
+                              [0,0,1]])
         # http://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
         a, b = norm(a), norm(b)
         v = cross(a, b)
         s = v.mag #sin of angle
-        c = a*b #cos of angle
+        c = dot(a, b) #cos of angle
 
         #creating [v]x, skew symmetric matrix
         #https://en.wikipedia.org/wiki/Skew-symmetric_matrix
@@ -101,37 +105,38 @@ class Coil(BInducer):
         return I + vx + (vx*vx) * (1-c)/(s**2)
 
 
-    def bfield_at(self, x, y, z):
+    def bfield_at(self, P):
         #translating center to origin and all associated points
-        newX, newY, newZ = x-self.center.x, y-self.center.y, z-self.center.z
+        newX, newY, newZ = P.x-self.center.x, \
+                           P.y-self.center.y, \
+                           P.z-self.center.z
 
         #rotating coil so normal vector is on z axis
-        newCoordinates = np.dot(rotate, np.matrix([[newX],[newY],[newZ]]))
+        transCoords = np.matrix([[newX],[newY],[newZ]])
+        newCoordinates = np.dot(self.rotate, transCoords)
         newX, newY, newZ = newCoordinates[0], newCoordinates[1], newCoordinates[2]
 
         #defining variables for equation
         #http://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/20010038494.pdf
         r = mag(vector(newX, newY, newZ)) #spherical coordinate equivalent of magnitude
-
         rhoSq = newX**2 + newY**2
         rSq = newX**2 + newY**2 + newZ**2
-        alphaSq = radius**2 + r**2 - 2*radius*(rhoSq**0.5)
-        betaSq = radius**2 + r**2 + 2*radius*(rhoSq**0.5)
+        alphaSq = self.radius**2 + r**2 - 2*self.radius*(math.sqrt(rhoSq))
+        betaSq = self.radius**2 + r**2 + 2*self.radius*(math.sqrt(rhoSq))
         kSq = 1 - (alphaSq/betaSq)
         gamma = newX**2 - newY**2
-        C = mu_0*I/math.pi
+        C = mu_0*self.I/math.pi
 
         #component calculations
-        Bx = (C*newX*newZ / (2*alphaSq*(betaSq**0.5)*rhoSq)) * \
-             ((radius**2 + r**2) * E(kSq) - alphaSq * K(kSq))
+        Bx = (C*newX*newZ / (2*alphaSq*(math.sqrt(betaSq))*rhoSq)) * \
+             ((self.radius**2 + r**2) * self.E(kSq) - alphaSq * self.K(kSq))
         By = Bx * (newY/newX)
-        Bz = (C/(2*alphaSq*(betaSq**0.5))) * \
-             ((radius**2 + r**2) * E(kSq) - alphaSq * K(kSq))
+        Bz = (C/(2*alphaSq*(math.sqrt(betaSq)))) * \
+             ((self.radius**2 + r**2) * self.E(kSq) - alphaSq * self.K(kSq))
 
         #rotating field components to original rotate state
-        finalB = np.dot(antiRotate, np.matrix([[Bx], [By], [Bz]]))
+        finalB = np.dot(self.antiRotate, np.matrix([[Bx], [By], [Bz]]))
         return vector(finalB[0], finalB[1], finalB[2])
 
 
-#class Solenoid: rectangular prism of coils
  
