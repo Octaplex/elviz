306aefe040655e24c9c09ee042bb43ac14f23671
Octaplex <damanm72@gmail.com>
2016-03-15 13:04:48 -0400

    misc fixes

--- shapes.py
+++ shapes.py
@@ -1,28 +1,17 @@
 from __future__ import division, print_function
 
 from visual import *
-
-from math import *
-
-import numpy as np
-import scipy as sp
-import numpy.linalg as la
-
-from scipy import integrate
-
+from math import pi, sin, cos
 from physics import *
 from constants import mu_0
+from util import K, E
 
 class Wire(BInducer):
     """
     A straight wire.
-
-    Wires connect two points A and B in three-dimensional space. In addition,
-    wires have scalar current I; the current flows from A to B if it is
-    positive, and from B to A if it is negative.
     """
 
-    def __init__(self, A, B, I, radius=0.1):
+    def __init__(self, A, B, I):
         # coordinates
         self.A = A
         self.B = B
@@ -36,7 +25,7 @@ class Wire(BInducer):
         self.I = self.BA.norm() * I
 
         # the rod
-        self.rod = cylinder(pos = self.A, axis = self.BA, radius=radius)
+        self.rod = cylinder(pos = self.A, axis = self.BA, radius = 0.1)
 
 
     def bfield_at(self, P):
@@ -47,15 +36,14 @@ class Wire(BInducer):
         # adapted from
         # http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
 
-
+        # get nearest point to wire
         t = -self.BA.dot(self.A - P) / self.l2
-        V = self.A + t*self.BA
-        r = P - V
+        r = P - (self.A + t*self.BA)
 
-        if r.mag == 0: return 0, vector(0, 0, 0)
+        # special case to avoid division by 0
+        if r.mag == 0: return vector(0, 0, 0)
 
-        # return magnitude and direction
-        return mu_0 * self.I.mag / (2*pi*r.mag), r.cross(V).norm()
+        return (mu_0 * self.I.mag / (2*pi*r.mag)) * r.cross(self.I).norm()
 
 
 class Coil(BInducer):
@@ -63,75 +51,70 @@ class Coil(BInducer):
     A coil of wire.
     """
 
-    def __init__(self, center, radius, normal, I):
+    def __init__(self, center, radius, normal, I, loops = 1, pitch = 1):
         self.center = center
         self.radius = radius
         self.normal = normal
+
+        self.loops = loops
+        self.pitch = pitch
+        self.length = loops*pitch
+
         self.I = I
+
+        # some consonants
+        self.C = mu_0*I/math.pi
+        self.oner = matrix([[1,0,0], [0,1,0], [0,0,1]])
+
         # rotation matrices for this coil, rotating so norm becomes z axis
-        rotate = self.find_rotmatrix(normal, vector(0, 0, 1))
-        antiRotate = inv(rotate)
+        self.rotate = self.find_rotmatrix(normal.norm(), vector(0, 0, 1))
+        self.antiRotate = linalg.inv(self.rotate)
 
-        ring(pos = center, axis = normal, radius = radius, thickness = 0.1)
-
-
-    # 1st and 2nd kind of elliptic integrals, K(k) and E(k) respectively
-    # http://www.mhtlab.uwaterloo.ca/courses/me755/web_chap3.pdf
-    def K(k):
-        return integrate.quad(lambda t: ((1-t**2)*(1-k**2*t**2))**-0.5, 0, 1)[0]
-
-    def E(k):
-        return integrate.quad(lambda t: ((1-k**2*t**2)/(1-t**2))**0.5, 0, 1)[0]
+        # the helix
+        if loops == 1:
+            self.helix = ring(pos = center, axis = normal*self.length,
+                    radius = radius, thickness = 0.1)
+        else:
+            self.helix = helix(pos = center, axis = normal*self.length,
+                    radius = radius, coils = loops, thickness = 0.1)
 
     def find_rotmatrix(self, a, b):
         # http://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
-        a, b = norm(a), norm(b)
         v = cross(a, b)
-        s = v.mag #sin of angle
-        c = a*b #cos of angle
 
-        #creating [v]x, skew symmetric matrix
-        #https://en.wikipedia.org/wiki/Skew-symmetric_matrix
-        vx = np.matrix([[0,    -v.z, v.y],
-                        [v.z,  0,    -v.x],
-                        [-v.y, v.x,  0]])
-        I = np.matrix([[1,0,0],
-                       [0,1,0],
-                       [0,0,1]])
-        return I + vx + (vx*vx) * (1-c)/(s**2)
+        # creating [v]x, skew symmetric matrix
+        # https://en.wikipedia.org/wiki/Skew-symmetric_matrix
+        vx = matrix([[ 0,  -v.z, v.y],
+                     [ v.z, 0,  -v.x],
+                     [-v.y, v.x, 0  ]])
+
+        return self.oner + vx + vx**2 * (1 - dot(a, b)) / v.mag2
 
 
-    def bfield_at(self, x, y, z):
-        #translating center to origin and all associated points
-        newX, newY, newZ = x-self.center.x, y-self.center.y, z-self.center.z
+    def bfield_at(self, P):
+        # translate center to origin and align normal to z axis
+        Pr = vector(dot(self.rotate, matrix(P - self.center).transpose()))
+        rx, ry, rz = Pr
 
-        #rotating coil so normal vector is on z axis
-        newCoordinates = np.dot(rotate, np.matrix([[newX],[newY],[newZ]]))
-        newX, newY, newZ = newCoordinates[0], newCoordinates[1], newCoordinates[2]
+        # defining variables for equation
+        # http://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/20010038494.pdf
+        r = Pr.mag
 
-        #defining variables for equation
-        #http://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/20010038494.pdf
-        r = mag(vector(newX, newY, newZ)) #spherical coordinate equivalent of magnitude
+        rho = (rx**2 + ry**2)**0.5
+        alpha = (self.radius**2 + r**2 - 2*self.radius*(rho))**0.5
+        beta = (self.radius**2 + r**2 + 2*self.radius*(rho))**0.5
+        gamma = rx**2 - ry**2
+        k = (1 - (alpha**2/beta**2))**0.5
 
-        rhoSq = newX**2 + newY**2
-        rSq = newX**2 + newY**2 + newZ**2
-        alphaSq = radius**2 + r**2 - 2*radius*(rhoSq**0.5)
-        betaSq = radius**2 + r**2 + 2*radius*(rhoSq**0.5)
-        kSq = 1 - (alphaSq/betaSq)
-        gamma = newX**2 - newY**2
-        C = mu_0*I/math.pi
+        if alpha == 0 or beta == 0 or rho == 0 or rx == 0: return vector(0, 0, 0)
 
-        #component calculations
-        Bx = (C*newX*newZ / (2*alphaSq*(betaSq**0.5)*rhoSq)) * \
-             ((radius**2 + r**2) * E(kSq) - alphaSq * K(kSq))
-        By = Bx * (newY/newX)
-        Bz = (C/(2*alphaSq*(betaSq**0.5))) * \
-             ((radius**2 + r**2) * E(kSq) - alphaSq * K(kSq))
+        # components
+        Bx = (self.C*rx*rz / (2*alpha**2*beta*rho**2)) * \
+             ((self.radius**2 + r**2) * E(k**2) - alpha**2 * K(k**2))
+        By = Bx * (ry/rx)
+        Bz = (self.C/(2*alpha**2*(beta))) * \
+             ((self.radius**2 + r**2) * E(k**2) - alpha**2 * K(k**2))
 
-        #rotating field components to original rotate state
-        finalB = np.dot(antiRotate, np.matrix([[Bx], [By], [Bz]]))
-        return vector(finalB[0], finalB[1], finalB[2])
-
-
-#class Solenoid: rectangular prism of coils
- 
+        # untranslate points and re-align to actual normal
+        B = vector(Bx, By, Bz)
+        return vector(dot(self.antiRotate, matrix(B).transpose()))
--- util.py
+++ util.py
@@ -1,4 +1,15 @@
 from __future__ import division, print_function
 
+from scipy import integrate
+
 def interpolate(color, val, base = (1, 1, 1)):
     return tuple(b + (c-b)*val for c, b in zip(color, base))
+
+# 1st and 2nd kind of elliptic integrals, K(k) and E(k) respectively
+# http://www.mhtlab.uwaterloo.ca/courses/me755/web_chap3.pdf
+def K(k):
+    return integrate.quad(lambda t: ((1-t**2)*(1-k**2*t**2))**-0.5, 0, 1)[0]
+
+def E(k):
+    return integrate.quad(lambda t: ((1-k**2*t**2)/(1-t**2))**0.5, 0, 1)[0]
+
