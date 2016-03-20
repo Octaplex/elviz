c418813d9e7c035fd1317a47616fc095365d8c37
jerryTheMonster <Jerrymeng100@gmail.com>
2016-03-20 15:58:14 -0400

    Math mostly fixed, drawing is off.
    All vectors are normalized, something wrong with the magnitude, but the
    directions are right.

--- shapes.py
+++ shapes.py
@@ -55,13 +55,6 @@ class Coil(BInducer):
         self.center = center
         self.radius = radius
         self.normal = normal
-<<<<<<< HEAD
-        self.I = I
-        # rotation matrices for this coil, rotating so norm becomes z axis
-        self.rotate = self.find_rotmatrix(normal, vector(0, 0, 1))
-        self.antiRotate = la.inv(self.rotate)
-=======
->>>>>>> origin/master
 
         self.loops = loops
         self.pitch = pitch
@@ -73,18 +66,11 @@ class Coil(BInducer):
         self.C = mu_0*I/math.pi
         self.oner = matrix([[1,0,0], [0,1,0], [0,0,1]])
 
-<<<<<<< HEAD
-    # 1st and 2nd kind of elliptic integrals, K(k) and E(k) respectively
-    # http://www.mhtlab.uwaterloo.ca/courses/me755/web_chap3.pdf
-    def K(self, k):
-        return integrate.quad(lambda t: ((1-t**2)*(1-k**2*t**2))**-0.5, 0, 1)[0]
-
-    def E(self, k):
-        return integrate.quad(lambda t: ((1-k**2*t**2)/(1-t**2))**0.5, 0, 1)[0]
-=======
         # rotation matrices for this coil, rotating so norm becomes z axis
-        self.rotate = self.find_rotmatrix(normal.norm(), vector(0, 0, 1))
-        self.antiRotate = linalg.inv(self.rotate)
+        self.rotate = matrix([[1,0,0],[0,1,0],[0,0,1]])
+        self.rotate = self.find_rotmatrix(normal.norm(), vector(0, 0, 1))    #TESTING
+        self.antiRotate = matrix([[1,0,0],[0,1,0],[0,0,1]])
+        self.antiRotate = linalg.inv(self.rotate)     #TESTING
 
         # the helix
         if loops == 1:
@@ -93,66 +79,10 @@ class Coil(BInducer):
         else:
             self.helix = helix(pos = center, axis = normal*self.length,
                     radius = radius, coils = loops, thickness = 0.1)
->>>>>>> origin/master
 
     def find_rotmatrix(self, a, b):
-        if a.x==0 and a.y==0: #if no rotation needed
-            return np.matrix([[1,0,0],
-                              [0,1,0],
-                              [0,0,1]])
         # http://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
         v = cross(a, b)
-<<<<<<< HEAD
-        s = v.mag #sin of angle
-        c = dot(a, b) #cos of angle
-
-        #creating [v]x, skew symmetric matrix
-        #https://en.wikipedia.org/wiki/Skew-symmetric_matrix
-        vx = np.matrix([[0,    -v.z, v.y],
-                        [v.z,  0,    -v.x],
-                        [-v.y, v.x,  0]])
-        I = np.matrix([[1,0,0],
-                       [0,1,0],
-                       [0,0,1]])
-        return I + vx + (vx*vx) * (1-c)/(s**2)
-
-
-    def bfield_at(self, P):
-        #translating center to origin and all associated points
-        newX, newY, newZ = P.x-self.center.x, \
-                           P.y-self.center.y, \
-                           P.z-self.center.z
-
-        #rotating coil so normal vector is on z axis
-        transCoords = np.matrix([[newX],[newY],[newZ]])
-        newCoordinates = np.dot(self.rotate, transCoords)
-        newX, newY, newZ = newCoordinates[0], newCoordinates[1], newCoordinates[2]
-
-        #defining variables for equation
-        #http://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/20010038494.pdf
-        r = mag(vector(newX, newY, newZ)) #spherical coordinate equivalent of magnitude
-        rhoSq = newX**2 + newY**2
-        rSq = newX**2 + newY**2 + newZ**2
-        alphaSq = self.radius**2 + r**2 - 2*self.radius*(math.sqrt(rhoSq))
-        betaSq = self.radius**2 + r**2 + 2*self.radius*(math.sqrt(rhoSq))
-        kSq = 1 - (alphaSq/betaSq)
-        gamma = newX**2 - newY**2
-        C = mu_0*self.I/math.pi
-
-        #component calculations
-        Bx = (C*newX*newZ / (2*alphaSq*(math.sqrt(betaSq))*rhoSq)) * \
-             ((self.radius**2 + r**2) * self.E(kSq) - alphaSq * self.K(kSq))
-        By = Bx * (newY/newX)
-        Bz = (C/(2*alphaSq*(math.sqrt(betaSq)))) * \
-             ((self.radius**2 + r**2) * self.E(kSq) - alphaSq * self.K(kSq))
-
-        #rotating field components to original rotate state
-        finalB = np.dot(self.antiRotate, np.matrix([[Bx], [By], [Bz]]))
-        return vector(finalB[0], finalB[1], finalB[2])
-
-
- 
-=======
 
         # creating [v]x, skew symmetric matrix
         # https://en.wikipedia.org/wiki/Skew-symmetric_matrix
@@ -188,6 +118,6 @@ class Coil(BInducer):
              ((self.radius**2 + r**2) * E(k**2) - alpha**2 * K(k**2))
 
         # untranslate points and re-align to actual normal
-        B = vector(Bx, By, Bz)
+        B = norm(vector(Bx, By, Bz))
+        #B = vector(Bx, By, Bz)
         return vector(dot(self.antiRotate, matrix(B).transpose()))
->>>>>>> origin/master
