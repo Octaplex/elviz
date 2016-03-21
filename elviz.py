bd065f5d187fd72474315ef3bafa6935a8da90ec
Octaplex <damanm72@gmail.com>
2016-03-21 08:00:44 -0400

    test wire

--- elviz.py
+++ elviz.py
@@ -4,11 +4,11 @@ from visual import *
 from physics import BField
 from shapes import Wire, Coil
 
-#w = Wire(vector(0, 0, 0), vector(0, 10, 0), 100000000)
+w = Wire(vector(0, 10, 0), vector(0, 0, 0), 1)
 
-r = Coil(vector(0, 0, 0), 10, vector(0, 1, 0), 10000000)
+#r = Coil(vector(0, 0, 0), 10, vector(0, 1, 0), 10)
 
-B = BField()
-B.add_inducer(r)
+B = BField(color = (0, 0.8, 0.2))
+B.add_inducer(w)
 
-B.draw(vector(-20, -20, -20), vector(20, 20, 20), 5)
+B.draw(vector(-10, 0, -10), (20, 20, 10), 2)
