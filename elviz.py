47b8842f20593aa6a4dba8d8d6fd28bc68fce3b7
Octaplex <damanm72@gmail.com>
2016-04-06 12:40:13 -0400

    do things

--- elviz.py
+++ elviz.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python2
 
-from visual import *
-from physics import BField
-from shapes import Wire, Coil
+from visual import vector, display
 
-#w = Wire(vector(0, 0, 0), vector(0, 1, 0), 100000000)
-r = Coil(vector(0, 1, 0), 2, vector(0, 1, 1), 100000000)
+from physics import *
+from shapes import *
+from ui import Elviz
 
-B = BField(color = (0, 0.8, 0.2))
-B.add_inducer(w)
+e = Elviz()
+d = e.scene
 
-B.draw(vector(-10, -10, -10), vector(20, 20, 20), 5)
+#w1 = Wire(vector(0, 0, 0), vector(0, 10, 0), 1, d)
+#w2 = Wire(vector(-3, 5, 4), vector(7, 10, 4), 2, d)
+#w3 = Wire(vector(1, 9, 6), vector(-3, -6, 2), 1, d)
+#r = Coil(vector(0, 1, 0), 2, vector(0, 1, 1), 1, d)
+
+B = BField(d, color = (0, 0.8, 0.2))
+#B.add_inducer(w1)
+#B.add_inducer(w2)
+#B.add_inducer(w3)
+#B.add_inducer(r)
+
+B.draw(vector(-10, 0, -10), vector(20, 10, 20), 5)
