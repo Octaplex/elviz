da2234ea33174e4dc49325d924c91083345d671c
Octaplex <damanm72@gmail.com>
2016-04-06 08:11:26 -0400

    add shape baseclass and preliminary ui stuff

--- shapes.py
+++ shapes.py
@@ -10,12 +10,53 @@ from physics import *
 from constants import mu_0, eps_0
 from util import K, E
 
-class Wire(BInducer, EInducer):
+class Shape:
+    """
+    The mostly abstract base class of all shapes.
+
+    Implements drag-and-drop behaviour. Subclasses must have an attribute
+    `obj' which refers to the VPython object that represents the shape.
+    """
+
+    def __init__(self, scene):
+        scene.bind('mousedown', self.grab)
+
+
+    def grab(self, evt):
+        """Grab the mouse and bind some more events."""
+        if evt.pick == self:
+            self.drag_pos = evt.pickpos
+            self.scene.bind('mousemove', self.move)
+            self.scene.bind('mouseup', self.drop)
+
+
+    def move(self, evt):
+        """Move the object when the mouse moves."""
+        new_pos = self.scene.mouse.project(normal=(0,0,1))
+        if new_pos != self.drag_pos:
+            self.moveto(new_pos - self.drag_pos)
+            self.drag_pos = new_pos
+
+
+    def drop(self, evt):
+        """Unbind events and clean up when the mouse is released."""
+        self.scene.unbind('mousemove', self.move)
+        self.scene.unbine('mouseup', self.drop)
+
+
+    def moveto(self, pos):
+        """Move the VPython object to a new position."""
+        self.obj.pos += pos
+
+
+class Wire(Shape, BInducer, EInducer):
     """
     A straight wire.
     """
 
     def __init__(self, A, B, I, scene):
+        Shape.__init__(self, scene)
+
         self.A = A
         self.B = B
 
@@ -25,7 +66,7 @@ class Wire(BInducer, EInducer):
 
         self.I = self.BA.norm() * I
 
-        self.rod = cylinder(pos = self.A, axis = self.BA, radius = 0.1,
+        self.obj = cylinder(pos = self.A, axis = self.BA, radius = 0.1,
                 display = scene)
 
 
@@ -55,12 +96,14 @@ class Wire(BInducer, EInducer):
         return (self.I / (2*pi*eps_0*self.l*r)) * r.norm()
 
 
-class Coil(BInducer):
+class Coil(Shape, BInducer):
     """
     A coil of wire.
     """
 
     def __init__(self, center, radius, normal, I, scene, loops = 1, pitch = 1):
+        Shape.__init__(self, scene)
+
         self.center = center
         self.radius = radius
         self.normal = normal
@@ -82,10 +125,10 @@ class Coil(BInducer):
         self.antiRotate = inv(self.rotate)     #TESTING
 
         if loops == 1:
-            self.helix = ring(pos = center, axis = normal*self.length,
+            self.obj = ring(pos = center, axis = normal*self.length,
                     radius = radius, thickness = 0.1, display = scene)
         else:
-            self.helix = helix(pos = center, axis = normal*self.length,
+            self.obj = helix(pos = center, axis = normal*self.length,
                     radius = radius, coils = loops, thickness = 0.1,
                     display = scene)
 
@@ -129,3 +172,49 @@ class Coil(BInducer):
         # untranslate points and re-align to actual normal
         B = vector(Bx, By, Bz).norm()
         return vector(self.antiRotate.dot(matrix(B).transpose()))
+
+
+class Bar(Shape, BInducer):
+    """
+    A static bar magnet.
+
+    Characteristics are as for the `box` object in VPython: pos is in the
+    center, axis defines the direction of the bar. To switch south and north
+    poles you can take the negative of the axis.
+    """
+
+    def __init__(self, pos, axis, moment, length, scene, height = 1, width = 0.5):
+        Shape.__init__(self, scene)
+
+        self.pos = pos
+        self.axis = axis
+
+        self.south = pos - axis*(length/2)
+        self.north = pos + axis*(length/2)
+        self.length = length
+
+        self.moment = moment*axis.rotate(angle=pi/2, axis=axis)
+
+        self.obj = box(pos = pos, axis = axis, length = length,
+                height = height, width = width, display = scene)
+
+
+    def bfield_from_pole(self, pole, P):
+        """
+        Calculate the bfield contribution from one pole of the magnet.
+
+        Adapated from:
+        https://en.wikipedia.org/wiki/Dipole
+        """
+
+        r = P - pole
+        rhat = r.norm()
+
+        # dirac weirdness
+        if r.mag2 == 0: return 0
+
+        return (mu_0/(4*pi))*(3*self.moment.dot(rhat)*rhat - self.moment)/(r.mag**3)
+
+
+    def bfield_at(self, P):
+        return self.bfield_from_pole(self.north, P) + self.bfield_from_pole(self.south, P)
