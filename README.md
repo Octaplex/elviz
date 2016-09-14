a4a9393cb469443990abcb464b42f18c3cc44cd3
Octaplex <damanm72@gmail.com>
2016-09-14 09:12:33 -0400

    merge README and Instructions + refactor elviz.py

--- README.md
+++ README.md
@@ -8,3 +8,47 @@ VPython provides downloads for
 [windows](http://vpython.org/contents/download_windows.html),
 [mac](http://vpython.org/contents/download_mac.html), and
 [linux](http://vpython.org/contents/download_linux.html).
+
+There isn't an installer yet, so to run Elviz you'll have to clone this repo.
+
+# Running
+
+## On the command line
+
+1. Navigate to the `elviz` directory, wherever you cloned it.
+2. (*optional*) Modify the file `elviz.py` to your liking (see [hacking]).
+3. Execute `python2 elviz.py` in a terminal (or whatever command your distro
+   uses for python 2).
+
+## In VIDLE
+
+VPython comes with a modification of Python's IDLE, called VIDLE, that can also
+be used to run Elviz:
+
+1. Open `elviz.py`, wherever you stored it.
+2. (*optional*) Modify the file to your liking (see [hacking])
+3. Click `Run > Run Module` to launch the program.
+
+# Hacking
+
+The sample `elviz.py` contains some objects that you can play around with, but
+it's much more fun to make your own scene! Some tips:
+
+- You can change properties of the scene by editing the setup section; for
+  example, you can change the color by editing the `BField` declaration (note:
+  while colors are in RGB format, each value ranges from 0 to 1).
+- Make sure you add any inducers you make to the `BField` itself, whatever you
+  name it (in the example, it is called `B`).
+- The call to `draw` takes the following arguments, in order:
+    - Origin (vector)
+    - Size (vector in <l, w, h> or scalar if all components are the same)
+    - Step (vector or scalar; how often to draw field lines)
+    - Radius (radius of drawing); leave empty for a rectangular drawing
+
+The following magnetic inducers are supported (look at `shapes.py` for more
+information):
+
+- Wire
+- Coil
+- Bar
+- Particle
--- elviz.py
+++ elviz.py
@@ -1,45 +1,37 @@
-################################################################################
-
-#DO NOT EDIT CONTENT ENCLOSED, THE CONSEQUENCES WILL BE SEVERE
-
 #!/usr/bin/python2
 
+# Imports (DO NOT EDIT)
 from visual import vector, display
-
 from physics import *
 from shapes import *
 from ui import Elviz
 
-################################################################################
-#Set width, height, and primary color
-e = Elviz(1600, 900)
+# Initial setup
+e = Elviz(1600, 900) # (width, height)
 d = e.scene
-B = BField(d, color = (1, 1, 0))
+B = BField(d, color = (1, 1, 0)) # color tuples are in (r, g, b) format
 
-#Edit here, after reading Instructions.txt
-#Wire(starting point, ending point, current, scene)
-#Coil(center, radius, normal vector, current, scene, loops(default=1), pitch(default=1))
-#Bar(starting point, direction, magnetic moment, length, scene, height(default=1), width(default=0.5))
-#Particle(center, magnetic moment*vector in direction of north pole, scene)
+# Constructor parameters:
+#
+# Wire(start, end, current, scene)
+# Coil(center, radius, normal vector, current, scene, loops(default=1), pitch(default=1))
+# Bar(start, direction, magnetic moment, length, scene, height(default=1), width(default=0.5))
+# Particle(center, magnetic moment, scene)
+#
+# See `shapes.py` for more precise definitions.
 
-#Example wire:
+# Example shapes
 #w = Wire(vector(0, -15, 15), vector(0, 15, -15), 1, d)
-
-#Example coil:
 r = Coil(vector(0, 0, 0), 10, vector(0, 1, 1), 10, d, 10, 0.5)
-
-#Example bar magnet:
 #bar = Bar(vector(10, 0, 0), vector(0, 0, 1), 1, 10, d)
-
-#Example dipole particle:
 #p = Particle(vector(0, 10, 0), 0.5*vector(0, -1, 0), d)
 
 
-#Add inducers
+# Add inducers
 #B.add_inducer(w)
 B.add_inducer(r)
 #B.add_inducer(bar)
 #B.add_inducer(p)
 
-#Set display field
+# Set display field
 B.draw(vector(-30, -30, -30), vector(60, 60, 60), 3, 30)
